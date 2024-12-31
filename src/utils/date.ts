9f5d5bf302f355219414a7bfffaa7383e87f5bf4
神代綺凛 <i@loli.best>
2025-01-01 01:37:23 +0800

    feat: export zip name contains hms

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,5 +3,8 @@ export const getDateString = () => {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
-  return `${year}${month}${day}`;
+  const hour = date.getHours().toString().padStart(2, '0');
+  const minute = date.getMinutes().toString().padStart(2, '0');
+  const second = date.getSeconds().toString().padStart(2, '0');
+  return `${year}${month}${day}${hour}${minute}${second}`;
 };
