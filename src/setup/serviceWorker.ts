886c0a414bf86d677e78b400d16e9e44b069944b
神代綺凛 <i@loli.best>
2025-07-29 01:17:48 +0800

    feat: update found notification

--- src/setup/serviceWorker.ts
+++ src/setup/serviceWorker.ts
@@ -1,12 +1,23 @@
 import { registerSW } from 'virtual:pwa-register';
 import SWMsgContent from '@/components/SWMsgContent.vue';
 
-const updateSW = registerSW({ onNeedRefresh: showUpdateMessage });
+const updateSW = registerSW({
+  onNeedRefresh: showUpdateMessage,
+  onRegistered(r) {
+    if (!r) return;
+    setInterval(() => r.update(), 3600e3);
+    r.addEventListener('updatefound', () => {
+      ElNotification.info({
+        title: 'New update found!',
+        message: 'Downloading...',
+      });
+    });
+  },
+});
 
 function showUpdateMessage() {
-  const handle = ElNotification({
+  const handle = ElNotification.primary({
     title: 'New update available!',
-    type: 'info',
     message: h(SWMsgContent, {
       onCancel: () => handle.close(),
       onConfirm: () => {
